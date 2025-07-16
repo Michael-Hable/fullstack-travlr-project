83907599d658f7ec95d2ef54feb56d00938d1d4d
Mike <mike.hable@gmail.com>
2025-07-16 16:21:53 -0400

    Module 3- Optional Challenge(about)

--- app_server/controllers/about.js
+++ app_server/controllers/about.js
@@ -1,6 +1,10 @@
+const fs = require('fs');
+const path = require('path');
+const aboutData = JSON.parse(fs.readFileSync(path.join(__dirname, '../../data/about.json'), 'utf8'));
+
 /* GET about view */
 const about = (req, res) => {
-  res.render('about', { title: 'Travlr Getaways' });
+  res.render('about', { title: 'Travlr Getaways', aboutData });
 };
 
 module.exports = {
