6a03af30a571bc83bd150afa357e0a5a9004c173
Mike <mike.hable@gmail.com>
2025-07-16 15:10:12 -0400

    Module 3- Optional Challenge(rooms,meals,news)

--- app_server/controllers/news.js
+++ app_server/controllers/news.js
@@ -1,6 +1,10 @@
+var fs = require('fs');
+var path = require('path');
+var newsData = JSON.parse(fs.readFileSync(path.join(__dirname, '../../data/news.json'), 'utf8'));
+
 /* GET news view */
 const news = (req, res) => {
-  res.render('news', { title: 'Travlr Getaways' });
+  res.render('news', { title: 'Travlr Getaways', newsData });
 };
 
 module.exports = {
