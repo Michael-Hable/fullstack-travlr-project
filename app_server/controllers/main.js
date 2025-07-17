e4b590258b87a6949567f2742df5951dd0856b46
Mike <mike.hable@gmail.com>
2025-07-16 20:16:01 -0400

    Module 3- Optional Challenge(home)

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -1,6 +1,10 @@
+const fs = require('fs');
+const path = require('path');
+const indexData = JSON.parse(fs.readFileSync(path.join(__dirname, '../../data/index.json'), 'utf8'));
+
 /* GET Homepage */
 const index = (req, res) => {
-  res.render('index', { title: 'Travlr Getaways' });
+  res.render('index', { title: 'Travlr Getaways', indexData });
 };
 
 module.exports = {
