47bd8942b47f74529311ac8f32c7994e1195f4bd
Mike <mike.hable@gmail.com>
2025-07-31 12:44:03 -0400

    Added search for single trip

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const router =express.Router();
 
+// This is where we import the controllers we will route
 const tripsController = require("../controllers/trips");
 
-router.route("/trips").get(tripsController.tripsList);
+// define route for our trips endpoint
+router
+  .route("/trips")
+  .get(tripsController.tripsList); // GET Method routes tripList
+
+// GET method routes tripsFindByCode - requires parameter
+router  
+  .route('/trips/:tripCode')
+  .get(tripsController.tripsFindByCode);
 
 module.exports = router;
